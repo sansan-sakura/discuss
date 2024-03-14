9e31a79160345e406a01f2cff9e50d87a4a5bcc5
Sakura Tanaka <sito6496t@gmail.com>
2024-03-14 10:09:03 +0900

    Finsih section7

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,12 @@
 "use server";
 
+import { auth } from "@/auth";
 import { z } from "zod";
+import type { Topic } from "@prisma/client";
+import { db } from "@/db";
+import paths from "@/path";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 const createTopicSchema = z.object({
   name: z
@@ -10,14 +16,62 @@ const createTopicSchema = z.object({
   description: z.string().min(10),
 });
 
-export async function createTopic(formData: FormData) {
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
   const result = createTopicSchema.safeParse({
     name: formData.get("name"),
     description: formData.get("description"),
   });
 
   if (!result.success) {
-    result.error.flatten().fieldErrors;
+    return { errors: result.error.flatten().fieldErrors };
   }
-  // revalidate the homepage
+
+  const session = await auth();
+
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in to do this"],
+      },
+    };
+  }
+
+  let topic: Topic;
+
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["something went wrong"],
+        },
+      };
+    }
+  }
+
+  revalidatePath("/");
+  redirect(paths.topicShow(topic.slug));
 }
